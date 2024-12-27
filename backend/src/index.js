275a38638eb41de0033ad2ef3cc14415c1f8ea32
elmestari_abdelmonim <126820006+Monim22@users.noreply.github.com>
2024-12-27 23:23:41 +0100

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
-import { fileURLToPath } from "url";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-// Set up __dirname equivalent in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",  // Ensure this is the correct URL for your frontend
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
@@ -28,18 +29,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  // Serve static files from the frontend folder (not dist)
-  app.use(express.static(path.join(__dirname, "../frontend")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  // Serve the index.html directly from the frontend folder
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-const PORT = process.env.PORT;
-
 server.listen(PORT, () => {
-  console.log("server running on port " + PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
